14cb4d912d29e64b3ad172898b38bb329021910f
leokingue <ebangkingue@icloud.com>
2023-10-22 18:01:24 +0200

    update the task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 '''
 0. Writing strings to Redis
 '''
@@ -25,3 +25,31 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, float, int]:
+        '''
+        get : Reading from Redis and recovering original type
+        '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''
+        Get a string from the cache.
+        '''
+        value = self._redis.get(key)
+        return value.decode('UTF-8')
+
+    def get_int(self, key: str) -> str:
+        '''
+        Get an int from the cache.
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('UTF-8'))
+        except Exception:
+            value = 0
+        return value
